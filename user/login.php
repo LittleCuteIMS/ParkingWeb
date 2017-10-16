33437508fdddcb1447ed9b426923b5f6d5067a54
nicokani <2917820775@qq.com>
2017-10-16 14:03:53 +0800

    新增登录及修改密码

---
+++ user/login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<?php
+/*
+ * 修改密码，接收来自客户端的json（原始密码，电话号码以及新密码）在数据库中进行查找比对，
+ * 若存在则进行新密码的修改，若不存在则反馈——原始密码错误
+ */
+include_once 'link1.php';
+$json = file_get_contents('php://input');  //接收json数据
+$arr = json_decode($json,true);
+//$json = '{"mobile":17867856434,"password":"636drf"}'; 
+//$arr=(array)json_decode($json);
+$userTelephone=$arr['mobile'];
+$passWord=$arr['password'];
+$sql="SELECT * FROM user WHERE mobile='$userTelephone' AND password='$passWord'";
+$result=mysqli_query($link, $sql); 
+if(mysqli_num_rows($result))
+{
+    echo 'true';
+}
+else{
+    echo 'false';
+}
+?>
