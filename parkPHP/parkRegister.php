92e4f0a26cef81530c86354a0d3b3246e6cd5e1e
RainOfPhone <792209198@qq.com>
2017-10-18 00:43:59 +0800

    增加了停车场注册页面的js文件，增加了重置按钮，修改了数据库管理的inser
    Signed-off-by: RainOfPhone <792209198@qq.com>

--- parkPHP/parkRegister.php
+++ parkPHP/parkRegister.php
@@ -1,6 +1,8 @@
 <?php
 ob_start();//开启输出缓冲
 //将表单提交的数据保存在$info数组中
+
+include '../mysql_db/Insert.php';
 $info=array(
     'name'=>$_POST['name'],
     'carportSum'=>(int)$_POST['carportSum'],
@@ -12,34 +14,16 @@ $info=array(
 //简单的输入检测
 $strName=(string)$info['name'];
 if($strName!=null){
-    //建立与数据库的连接
-    $mysqli = new mysqli('localhost','root','','intelligentparking');
+    $insertpark=new Insert();
     
-    //连接出错时
-    if($mysqli->connect_error){
-        die('连接错误('. $mysqli->connect_errno . ')'
-            .$mysqli->connect_error);
-    }
-    
-    //将注册数据插入park表的sql语句
-    $sql="insert into park(name,carport_sum,carport_free_num,address,phone,charge) values ('"
-        .$info['name']."','".$info['carportSum']."','".$info['carportFreeNum']."','"
-            .$info['address']."','".$info['phone']."','".$info['charge']."')";
-    
-    //避免存入数据库乱码
-    $mysqli->query("set character set 'UTF8'");
-    $mysqli->query("set names 'UTF8'");
-    
-    //检测插入是否成功
-    $result=$mysqli->query($sql);
+    $result=$insertpark->insert_park($info['name'], $info['carportSum'], 
+        $info['carportFreeNum'], $info['address'], $info['phone'], $info['charge']);
+
     if($result){
         echo '注册成功</a>';
     }else{
         echo '注册失败';
     }
-    
-    //关闭连接
-    $mysqli->close();
 }else{
     echo '输入数据有误，请重新输入'.'<br />';
 }
