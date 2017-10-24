85dac5ea81ceaaf2f776c4916a2ad390971e6322
nicokani <2917820775@qq.com>
2017-10-24 15:29:01 +0800

    修改

--- user/dateinquiry.php
+++ user/dateinquiry.php
@@ -7,13 +7,6 @@
 <body>
 
 		<div>
-<table border="1"  align="center" width="80%" height="60%">
-         <tr  valign="middle" height="40" align="center" > 
-         <td width=30%>注册日期</td>
-         <td width="10%">id</td>
-         <td width="30%">昵称</td>
-         <td width="30%">手机号码</td>
-         </tr>
     <?php
     /*
      * 根据给出的时间范围查询用户信息
@@ -23,16 +16,25 @@
        $date1=$_GET['date1'];
        $date2=$_GET['date2'];  
         $sql="SELECT * FROM user where regis_date between '$date1' AND '$date2'";
-         $result = mysqli_query($link,$sql);
-         $datarow = mysqli_num_rows($result);          //长度
+         $result = mysqli_query($link,$sql);    
+         $datarow = mysqli_num_rows($result); //长度
+         if ($datarow>0)
+         {
+             echo "<table border='1' align='center' width='80%' height='60%'>
+             <tr  valign='middle' height='40' align='center' >
+             <td width=30%>注册日期</td>
+             <td width='10%'>id</td>
+             <td width='30%'>昵称</td>
+             <td width='30%'>手机号码</td>
+             </tr>";
             //循环遍历出数据表中的数据
-            for($i=0;$i<$datarow;$i++)
+            for ($i=0;$i<$datarow;$i++)
             {
                 $result_arr = mysqli_fetch_assoc($result);
                 $userId = $result_arr['id'];
                 $nickName = $result_arr['nickname'];    
                 $regisDate= $result_arr['regis_date'];
-                $mobile=$result_arr['mobile'];
+                $mobile=$result_arr['mobile'];           
                 echo  "<tr  valign='middle' height='40' align='center'>
                        <td width=30%>$regisDate</td>
                        <td width='10%'>$userId</td>
@@ -40,6 +42,9 @@
                        <td width='30%'>$mobile</td>
                        </tr>";
             }
+         }else{
+             echo "请输入正确的查询时间！";
+         }
             mysqli_close($link);
      ?>
 
