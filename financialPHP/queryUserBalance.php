719b461c42a32f6aefc17df80fd1048124023d18
zhanglin <1003677516@qq.com>
2017-11-29 13:10:29 +0800

    修改 查询余额

--- financialPHP/queryUserBalance.php
+++ financialPHP/queryUserBalance.php
@@ -1,16 +1,20 @@
 <?php
-	include '../mysql_db/mysqliBySql.php';
-	
-	$json = file_get_contents('php://input');
-	$arr = json_decode($json,true);
-	$userPhone=$arr['mobile'];
-    $sql="select balance from user where mobile='".$userPhone."'";
-    $jsonData=selectBySql($sql);//查询结果为json格式数据
-	if($jsonData != false){
-		$userinfo=json_decode($jsonData,true);
-		$balance=$userinfo[0]['balance'];
-		echo $balance;
-	}else{
-	    echo "0";
-	}
+/*
+ * 查询余额
+ */
+include_once '../user/link1.php';
+$json = file_get_contents('php://input');  //接收json数据
+$arr = json_decode($json,true);
+//$json = '{"mobile":18782003437}'; 
+//$arr=(array)json_decode($json);
+$userTelephone=$arr['mobile'];
+$sql="SELECT * FROM user WHERE mobile='".$userTelephone."' ";
+$result=mysqli_query($link, $sql); 
+if($row = mysqli_fetch_array($result))
+{
+    echo $row['balance'];
+}
+else{
+    echo 'FAILURE';
+}
 ?>
