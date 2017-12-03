a354135d7effd6f6b590635481a49eeea42f6180
nicokani <2917820775@qq.com>
2017-12-03 19:50:47 +0800

    停车历史记录由车牌查询改为手机号查询

--- parkPHP/parking_record_select.php
+++ parkPHP/parking_record_select.php
@@ -1,8 +1,7 @@
 <?php
 /*
- * （需要接收的数据为车牌号）
+ * （需要接收的数据为用户车牌号）
  * 根据车牌号查询停车记录(数据应该是会返回给手机的）
- * 注：这里只能根据车牌号查询，因为一个电话号码可以对应多个车牌号
  * 
  * written by 荣誉婷on Nov 25,2017
  * 
@@ -11,12 +10,20 @@ include_once '../user/link1.php';
 include_once '../mysql_db/mysqliBySql.php';
 $json = file_get_contents('php://input');  //接收json数据
 $arr = json_decode($json,true);
-//$json ='{"plate_number":"川B23123"}';
+//$json ='{"mobile":"18281623534"}';
 //$arr=(array)json_decode($json);
-$plateNumber=$arr['plate_number'];
-//echo $plateNumber;
-$sql="select * from parking_record where plate_number='".$plateNumber."'";
-$out=selectBySql($sql);
+$userTelephone=$arr['mobile'];
+//echo $userTelephone;
+$sql1="select user.id from user where mobile='$userTelephone'";
+$result1=mysqli_query($link, $sql1);
+$row1=mysqli_fetch_assoc($result1);
+$userId=$row1['id'];
+$sql2="select car.plate_number from car where user_id='$userId'";
+$result2=mysqli_query($link, $sql2);
+$row2=mysqli_fetch_assoc($result2);
+$plateNumber=$row2['plate_number'];
+$sql3="select * from parking_record where plate_number='".$plateNumber."'";
+$out=selectBySql($sql3);
 if($out==true)
 {
     echo $out;
