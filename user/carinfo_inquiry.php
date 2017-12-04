af769b303b5e7eabc74a3ce7eba5d710608dd986
nicokani <2917820775@qq.com>
2017-12-04 10:54:56 +0800

    车辆信息返回时新增了remarks数据

--- user/carinfo_inquiry.php
+++ user/carinfo_inquiry.php
@@ -28,7 +28,8 @@ if($datarow>=1)                                     //查询的行数不为0，�
      { 
          $result_arr = mysqli_fetch_assoc($result);
          $plateNumber=$result_arr['plate_number'];
-         $jsonarr=array('plate_number'=>$plateNumber);//将带输出内容放到数组中，以键值对的形式，方便后面转化成json数组
+         $remark=$result_arr['remarks'];
+         $jsonarr=array('plate_number'=>$plateNumber,'remarks'=>$remark);//将带输出内容放到数组中，以键值对的形式，方便后面转化成json数组
          $json=json_encode($jsonarr,JSON_UNESCAPED_UNICODE);//转换成json数组
          echo $json;
      }
